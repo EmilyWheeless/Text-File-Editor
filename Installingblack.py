6887c6f3ade1308eff40aafca2e6bfa980be7210
Emily <wheelessemily@gmail.com>
2025-06-02 10:50:31 -0500

    Add feature to loop exit and quit

--- Installingblack.py
+++ Installingblack.py
@@ -1,2 +1,28 @@
 # Converting Text File Indentations to Python Indentations
 
+import black
+
+def check_software_status():
+    try:
+        import black
+        print('Black is available!')
+    except ImportError:
+        print('Black not installed properly.')
+
+def code_formatting(file_path):
+    try: 
+        with open(file_path, 'r') as file:
+            translation=file.read()
+            formatted_code = black.format_str(translation, mode=black.FileMode())
+        with open(file_path, 'w') as file:
+            file.write(formatted_code)
+            print(f'Formatted {file_path} successfully!')
+    except Exception as e:
+        print(f'File {file_path} was unable to be translated properly.')
+        print(f'Error: {e}') 
+
+file_path=input(f'Please Input Raw Code File Here :] *.txt with python only plz* --> ')
+
+code_formatting(file_path)
+
+
