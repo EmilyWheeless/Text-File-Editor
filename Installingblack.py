af19c29ee6211f88f9b41786b1e7a56531616ab5
Emily <wheelessemily@gmail.com>
2025-06-02 10:55:54 -0500

    added new loop features

--- Installingblack.py
+++ Installingblack.py
@@ -2,13 +2,6 @@
 
 import black
 
-def check_software_status():
-    try:
-        import black
-        print('Black is available!')
-    except ImportError:
-        print('Black not installed properly.')
-
 def code_formatting(file_path):
     try: 
         with open(file_path, 'r') as file:
@@ -17,12 +10,22 @@ def code_formatting(file_path):
         with open(file_path, 'w') as file:
             file.write(formatted_code)
             print(f'Formatted {file_path} successfully!')
+        return True #Format was successful :3
     except Exception as e:
         print(f'File {file_path} was unable to be translated properly.')
         print(f'Error: {e}') 
+        return False #Format FAILED D:
+    
+def clean_input(file_path):
+    return file_path.strip('')
+while True:
+    file_path = input(f'Please Input Raw Code File Here :] *file with python code only plz* --> ')
+    file_path = clean_input(file_path) #Found that clean_input easier to use than classic if statements :D
+    if file_path.lower() == 'exit' or file_path.lower() == 'quit':
+        print(f'Goodbye!')
+        break 
+    if code_formatting(file_path):
+        break 
 
-file_path=input(f'Please Input Raw Code File Here :] *.txt with python only plz* --> ')
-
-code_formatting(file_path)
 
 
